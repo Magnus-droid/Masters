17d1f94397e719cd712f81d4d0f1aac7edfb5de7
Magnus-droid <magnuskrumbacher@gmail.com>
2024-03-21 11:24:17 +0000

    cloud console view

--- imports/color_strings.py
+++ imports/color_strings.py
@@ -1,6 +1,6 @@
 
 
-color_strings = {"help":
+color_strings = {"help-vehicle":
 """\033[0m\033[1m\033[31mHelp :
 \033[0m\033[36mcmd \033[0m\033[33m[option1|option2]{string}(number) \033[0m
 
@@ -79,5 +79,36 @@ Stack trace:
 "welcome":
 """\033[1m\033[35mWelcome on console
 
+
+""",
+
+"help-cloud":
+"""\033[0m\033[1m\033[31mWelcome on console\033[0m
+
+\033[0m\033[1m\033[31mHelp :\033[0m
+\033[0m\033[36mcmd \033[0m\033[33m[option1|option2]{string}(number) \033[0m
+
+\033[1m\033[31mBuiltins :\033[0m
+\033[0m\033[36mcversion \033[0m\033[33m\033[0m          Console version
+\033[36mhelp \033[0m\033[33m\033[0m               Display help
+\033[36mscreen \033[0m\033[33m[(X)] \033[0m       Change to screen X. If no argument, display screen list
+\033[36mcolor \033[0m\033[33m[0|1] \033[0m        Enable/Disable color output
+\033[36mlang \033[0m\033[33m[{str}] \033[0m       Set the console language
+\033[36mreboot \033[0m\033[33m[(waitTime)] \033[0mReboot
+\033[36mcompletion \033[0m\033[33m\033[0m         Activate advanced completion
+\033[36mexit \033[0m\033[33m\033[0m               Quit
+
+\033[1m\033[31mVehicle Management :\033[0m
+\033[36mstatus \033[0m\033[33m\033[0m             Display vehicle status and diagnostics
+\033[36mgpspos \033[0m\033[33m\033[0m             Retrieve last GPS position of vehicles
+\033[36mtelemetry \033[0m\033[33m\033[0m          View telemetry data from vehicle sensors
+\033[36mdiagnostics \033[0m\033[33m\033[0m        Run diagnostic tests on vehicle systems
+\033[36mfirmware \033[0m\033[33m\033[0m           Update vehicle firmware
+
+\033[1m\033[31mCloud Services :\033[0m
+\033[36minstances \033[0m\033[33m\033[0m          Manage cloud instances and virtual machines
+\033[36mstorage \033[0m\033[33m\033[0m            Manage cloud storage resources
+\033[36mnetworking \033[0m\033[33m\033[0m         Configure networking settings for cloud deployments
+\033[36msecurity \033[0m\033[33m\033[0m           Monitor and manage security configurations
 
 """}
