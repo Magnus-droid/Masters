49891d9fe0ad08702000e8bd646fb0af22032273
Magnus-droid <magnuskrumbacher@gmail.com>
2024-04-10 10:47:42 +0000

    added session logging back

--- imports/cleanup.py
+++ imports/cleanup.py
@@ -20,10 +20,10 @@ def update_command_counts(command_summary_file, new_command_counts):
         for command, count in existing_command_counts.items():
             command_summary.write(f"{command}: {count}\n")
 
-def generate_summary(logs_dir, ip_summary_file, command_summary_file):
+def generate_summary(logs_dir, ip_summary_file, command_summary_file, combined_file):
     # Dictionary to store counts of commands attempted
     command_counts = {}
-
+    ip_commands = {}
     # Set to store unique attacker IPs
     attacker_ips = set()
 
@@ -37,6 +37,8 @@ def generate_summary(logs_dir, ip_summary_file, command_summary_file):
             # Read commands from the log file
             with open(filepath, 'r') as file:
                 commands = file.readlines()
+            
+            ip_commands[attacker_ip] = commands
 
             # Update count for each command
             for command in commands:
@@ -51,6 +53,12 @@ def generate_summary(logs_dir, ip_summary_file, command_summary_file):
     with open(ip_summary_file, 'a') as ip_summary:
         for ip in attacker_ips:
             ip_summary.write(ip + '\n')
+
+    with open(combined_file, 'a') as combined:
+        for ip, commands in ip_commands.items():
+            combined.write(f"[{ip}]\n")
+            combined.writelines(commands)
+            combined.write("------------------\n\n")
 
     # Update command summary file with new counts
     update_command_counts(command_summary_file, command_counts)
@@ -67,6 +75,8 @@ ip_summary_file = "/home/ubuntu/Masters/logs/ip_summary_file.txt"
 # File to store summary of command attempts
 command_summary_file = "/home/ubuntu/Masters/logs/command_summary_file.txt"
 
+combined_file = "/home/ubuntu/Masters/logs/combined_file.txt"
+
 # Generate the summary
-generate_summary(logs_directory, ip_summary_file, command_summary_file)
+generate_summary(logs_directory, ip_summary_file, command_summary_file, combined_file)
 
